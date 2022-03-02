a1c8aeb9f265344929c7e3a915a5ba989627b46a
Jignesh <jigsshah.97@gmail.com>
2022-03-02 19:26:42 +0530

    Scheduling news and int_fixtures done

--- homepage/views.py
+++ homepage/views.py
@@ -26,7 +26,7 @@ def storing_news_in_db():
 #Fetching and storing news in database every 30 minutes
 def news_scheduler():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(storing_news_in_db, 'interval', minutes = 2)
+    scheduler.add_job(storing_news_in_db, 'interval', minutes = 30)
     scheduler.start()
 
 #storin international fixtures in the database
@@ -43,13 +43,13 @@ def storing_int_fixtures_in_db():
             int_fix_date = Fixtures_date(fixture = int_fixture, date = i[0])
             int_fix_date.save()
             print(f'Updating International Fixtures.....')
-    current_time = datetime.now(timezone("Asia/Kolkata")).strftime('%Y-%m-%d %H:%M:%S.%f')
+    current_time = datetime.now(timezone("Asia/Kolkata")).strftime('%Y-%ms-%d %H:%M:%S.%f')
     print(f'Checked for updated Fixtures at {current_time}')
 
 #Fetching and storing updated international fixture in database every 24 hours
 def int_fixture_scheduler():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(storing_int_fixtures_in_db, 'interval', minutes = 1)
+    scheduler.add_job(storing_int_fixtures_in_db, 'interval', hours = 24)
     scheduler.start()
 
 ########################################## VIEWS SECTION ##########################################
@@ -83,16 +83,12 @@ def detailed_news(request, *arg, **kwargs):
     return render(request, 'homepage/detailed_news.html', context)
 
 def fixtures(request):
-    '''
-    int_fixtures = Fixtures.objects.filter(fixture_type = 'international').order_by('fixture_id')
-    #print(int_fixtures)
-    int_fixtures_date = Fixtures_date.objects.filter(fixture__in = int_fixtures)
-    #print(int_fixtures_date)
-    context = {
-        'int_fixtures': int_fixtures,
-        'int_fixtures_date': int_fixtures_date,
-    }
-    '''
+    today = datetime.today()
+    date_from, month, year = today.strftime("%d"), today.strftime("%b").upper(), today.strftime("%Y")
+    print(f'{date_from} {month} {year}')
+    date_to = str(int(date_from) + 2).zfill(2)
+
+
     return render(request, 'homepage/fixtures.html')
 
 def login(request):
