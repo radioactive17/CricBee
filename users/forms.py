78203abb246a7acd1d114acb7fbd6d66088ad7ff
Jignesh <jigsshah.97@gmail.com>
2022-03-10 19:59:59 +0530

    User(Bees) module completed

--- users/forms.py
+++ users/forms.py
@@ -3,8 +3,23 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
 from .models import Bees
+
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'password1', 'password2', 'email']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email',]
+
+class BeesUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Bees
+        fields = ['image']
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm, BeesUpdateForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def register(request):
@@ -17,3 +18,23 @@ def register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form':form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance = request.user)
+        b_form = BeesUpdateForm(request.POST, request.FILES, instance = request.user.bees)
+        if u_form.is_valid() and b_form.is_valid():
+            u_form.save()
+            b_form.save()
+            messages.success(request, 'Profile Updated Successfully')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance = request.user)
+        b_form = BeesUpdateForm(instance = request.user.bees)
+
+    context = {
+        'u_form': u_form,
+        'b_form': b_form,
+    }
+    return render(request, 'users/profile.html', context)
